4c1a171bbb2e135e121ffa9a203f1a8eeb8bf7e3
duanyongcheng <1171998654@qq.com>
2025-06-04 20:52:42 +0800

    feat: new keymaps

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -1,17 +1,11 @@
--- [[ Basic Keymaps ]]
---  See `:help vim.keymap.set()`
+local keymap = vim.keymap
 
--- Clear highlights on search when pressing <Esc> in normal mode
---  See `:help hlsearch`
-vim.keymap.set('n', 'J', '10j')
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
--- vim.keymap.set('n', '<leader>q', '<cmd>wqall<CR>', { desc = 'quick exit whith save' })
--- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+keymap.set('n', '<space>qq', '<cmd>wqall<cr>', { desc = 'Smart Find Files' })
+keymap.set('n', 'J', '10j')
+keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+keymap.set('n', '<space>m', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
